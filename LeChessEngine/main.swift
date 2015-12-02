bdcdc46b86f26bb7fa632daf83ec1fd976df31a8
Leigh Appel <leigh@safesiteapp.com>
2015-12-03 09:03:50 +1000

    Initial Commit

---
+++ LeChessEngine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeChessEngine
+//
+//  Created by Leigh Appel on 3/12/2015.
+//  Copyright © 2015 JasperVine. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
